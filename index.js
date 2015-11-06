cd08475a0c0002841a6969a9dd7cc836e55ead80
Madhav Bhagat <madhav@Madhavs-MacBook-Pro.local>
2015-11-06 18:44:58 +0530

    automatically set json

--- index.js
+++ index.js
@@ -119,6 +119,9 @@ var define = function(functionName, handler) {
 
 var httpRequest = function(options) {
   var promise = new Parse.Promise();
+  if (typeof options.body == 'Object') {
+    options.json = true;
+  }
   Request(options, function(error, response, body) {
     if (error) {
       console.log('Parse.Cloud.httpRequest failed with error:');
